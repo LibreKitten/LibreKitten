2e8b14cbbe60e07dae2351a963822933a83fbc74
ACat <acat@noreply.codeberg.org>
2025-07-23 15:18:42 +1000

    feat(gui): add phone detection

--- packages/gui/src/lib/isPhone.ts
+++ packages/gui/src/lib/isPhone.ts
@@ -1,8 +1,7 @@
 const isPhone = () => {
-    // lk: TODO: Actually detect if the device is a phone.
-    let isMobile: boolean = false; 
     if (new URLSearchParams(window.location.search).has('forceisphone')) return true;
-    return isMobile;
+    // Credit to https://medium.com/@rchr/detecting-mobile-browsers-with-one-line-of-javascript-109713d5869c
+    return Math.min(window.screen.width, window.screen.height) < 768;
 }
 
 export default isPhone;
