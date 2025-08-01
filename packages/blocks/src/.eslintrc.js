c495c56fd4a2d5e6f8a0efbf230ffddca2dcb56d
ACat <acat@noreply.codeberg.org>
2025-08-01 15:39:02 +1000

    refactor(blocks): add `src` directory

---
+++ packages/blocks/src/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": ["scratch", "scratch/es6"],
+    "rules": {
+        // This seems to error, so we have to turn it off.
+        "require-atomic-updates": "off"
+    }
+}
