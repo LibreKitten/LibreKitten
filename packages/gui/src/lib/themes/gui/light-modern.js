213cedddb3b74c565cb74b75eba63a6458961dbf
ACat <acat@noreply.codeberg.org>
2025-08-25 12:48:56 +1000

    feat(gui): lower the blue tint in light-modern theme

--- packages/gui/src/lib/themes/gui/light-modern.js
+++ packages/gui/src/lib/themes/gui/light-modern.js
@@ -1,6 +1,10 @@
 import logo from '../../../lib/assets/librekitten-colour.svg';
 
 const guiColors = {
+    'ui-primary': 'hsla(215, 45%, 96%, 1)',
+    'ui-secondary': 'hsla(215, 45%, 94%, 1)',
+    'ui-tertiary': 'hsla(215, 45%, 92%, 1)',
+
     'menu-bar-background': 'rgba(255, 255, 255, 1)',
     'menu-bar-foreground': 'rgba(0, 0, 0, 1)',
 
