e4418666571776f0ec91225b46808166e2c2042b
ACat <acat@noreply.codeberg.org>
2025-10-25 14:13:06 +1100

    feat(gui): update tips

--- packages/gui/src/lib/randomUnhelpfulTip.js
+++ packages/gui/src/lib/randomUnhelpfulTip.js
@@ -1,20 +1,39 @@
+/* eslint-disable max-len */
 export const tip = [
-    'LibreKitten was started at January 6, 2024.',
-    'Fact: Capybaras and cats rule, humans drool.',
-    'The name of the LibreKitten cat is "Librekitty".',
-    'Tip: If you\'re having trouble with your project, ask someone for help.',
-    'Tip: These tips are unhelpful.',
-    'Fact: Your code is the cat\'s meow, keep going.',
-    'Fact: This is a sentence.',
-    'Fact: Capybaras look chill, but they\'re actually not, they\'re planning WORLD DOMINATION.',
-    'Crazy? I was crazy once. They locked me in a room, a rubber room. A rubber room with rats. And rats make me crazy.',
-    'Meow! Purrrrrr. :3',
+    // Related to the software, and tips
+    'LibreKitten was started on January 6, 2024.',
+    'Fun fact: LibreKitten is free software released under a mixture of GNU GPL v3 and MPL v2.0.',
+    'Tip: Some blocks have descriptions; you can hover over a block to see it (if it has one).',
+    'Tip: You can use the "Web Server" extension to make simple APIs and websites.',
+    'LibreKitten is in alpha, so there may be bugs and breaking changes.',
+
+    // Encouragement
+    'If you\'re having trouble with your project — you can always ask someone for help.',
+    'Fact: Your code is the <em>cat\'s meow</em>, keep going!',
+
+    // Promotion
     'Also check out <a href="https://turbowarp.org">TurboWarp</a>!',
     'Also check out <a href="https://alpha.unsandboxed.org">Unsandboxed</a>!',
+    'Also check out <a href="https://ampmod.codeberg.page">AmpMod</a>!',
+
+    // Character or lore related
+    'The name of the LibreKitten cat mascot is "Librekitty".',
+    'Did you know: LibreKitten has LORE!?1/!?1/!!?',
+    'Lore: The Librekitty has a sister named Psipsina.',
+    'Lore: The Librekitty is not a cat, robot, nor a lifeform.',
+    'Lore: Psipsina is <em>very</em> misanthropic, while the Librekitty is very much the opposite.',
+    'oOH!! PsiPsINa sIPs hEr SiPPy CuP!1',
+
+    // Random or silly
+    'Truth: Capybaras and cats rule, humans drool.',
+    'Truth: This is a sentence.',
+    'Fun fact: Capybaras look chill, but they\'re actually not, they\'re planning WORLD DOMINATION.',
+    'Crazy? I was crazy once. They locked me in a room, a rubber room. A rubber room with rats. And rats make me crazy.',
+    'Meow! Purrrrrr. :3',
     'Cats are cute.',
     'Look behind you >:3',
     'Fun fact: Every website recieves your IP address.',
-    'I know what you did on January 6th 2015.',
-    '<iframe width="200" height="112" src="https://www.youtube-nocookie.com/embed/sRuCvdT8H_Y?si=v248pEf_sXgEDMBq&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+
+    // Miscellaneous
     '<b>Did you know?</b> LGBT Pride Month is celebrated every June.'
 ];
